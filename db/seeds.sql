0867ea8aa938eb6f853a81c3b2a7fb4fe9363eb2
Robert Cortez <Nuhuhfoo@gmail.com>
2018-06-22 08:59:45 -0700

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Burger', true);
